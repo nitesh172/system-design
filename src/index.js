4db075b0841f7f71b5678cd9fbf1fab29e9ae566
nitesh172 <niteshbaghel172@gmail.com>
2025-07-06 12:57:10 +0530

    Update index file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express Server!")
+  res.status(200).send("Hello from Express!")
 })
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}`)
 })
